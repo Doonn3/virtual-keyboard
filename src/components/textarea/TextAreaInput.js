2e1215262f4f506aee04f24ee1bc5564a498d3c4
Николай <Doonn3@gmail.com>
2023-05-01 21:58:02 +0300

    refactor: TextAreaInput

--- src/components/textarea/TextAreaInput.js
+++ src/components/textarea/TextAreaInput.js
@@ -1,31 +1,39 @@
 import VirtualKeyboardEvent from '../../Events/VirtualKeyboardEvent';
 import BaseCommand from './Command/BaseCommand';
+import './style.scss';
 
 class TextAreaInput {
   #command;
-  #isFocus = false;
+
+  #isFocus = true;
+
   init() {
-    const root = document.querySelector('.text');
+    const root = document.querySelector('.text-area');
     this.#command = new BaseCommand(root);
     root.addEventListener('input', this.handlerTextArea);
-    root.addEventListener('focus', this.#handlerFocus);
-    root.addEventListener('focusout', this.#handlerFocusout);
+    // root.addEventListener('focus', this.#handlerFocus);
+    // root.addEventListener('focusout', this.#handlerFocusout);
+    root.focus();
+    root.addEventListener('blur', () => {
+      root.focus();
+    });
 
     VirtualKeyboardEvent.current.subscribe(this.#handlerVirtualKeyboard);
   }
 
   render() {
-    return `<textarea class="text"></textarea>`;
+    return '<textarea class="text-area"></textarea>';
   }
 
   #handlerVirtualKeyboard = (data) => {
     if (this.#command instanceof BaseCommand) {
       this.#command.execute(data);
     }
-  }
+  };
 
   handlerTextArea = (event) => {
     if (this.#isFocus) return;
+    alert('s');
     if (event instanceof InputEvent) return;
 
     if (this.#command instanceof BaseCommand) {

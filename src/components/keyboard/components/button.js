83d70d8dd16a91b5ca8e39480e59fc4e015e4357
Николай <Doonn3@gmail.com>
2023-05-01 21:57:54 +0300

    refactor: button

--- src/components/keyboard/components/button.js
+++ src/components/keyboard/components/button.js
@@ -1,13 +1,29 @@
 import './button.scss';
 
+const check = [
+  'Backspace',
+  'Tab',
+  'Delete',
+  'CapsLock',
+  'Enter',
+  'ShiftLeft',
+  'ShiftRight',
+  'ControlLeft',
+  'ControlRight',
+  'MetaLeft',
+  'AltLeft',
+  'AltRight',
+];
+
 function button(buttonType) {
   const { id, key, dataEng, dataRus } = buttonType;
 
-  const classNameMod = id === 'Tab' ? 'tab' : '';
+  const classNameMod =
+    id === check.find((item) => item === id) ? 'medium' : id === 'Space' ? 'large' : '';
 
   return `
     <div id="${id}" class="btn ${classNameMod}" data-key-eng="${dataEng}" data-key-rus="${dataRus}">
-        <p class="btn__key">${key}</p>
+        <span class="btn__key">${key}</span>
     </div>
     `;
 }
